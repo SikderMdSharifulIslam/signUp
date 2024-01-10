1966d6f62cd30e091253f4e98d1453393a6a1b7a
sikdermdsharifulislam <sikdermdsharifulislam@gmail.com>
2024-01-10 18:04:20 +0600

    data fetched from database

--- view.php
+++ view.php
@@ -2,6 +2,16 @@
 <?php
 include 'includes/header.php';
 include 'includes/navbar.php';
+
+// Database Connection
+define("HOSTNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DATABASE_NAME", "sign_up");
+$db_connect = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE_NAME);
+
+$select_query = "SELECT id,full_name,email_address FROM users";
+$data_from_db = mysqli_query($db_connect, $select_query);
 ?>
 <!-- Header Part End -->
 
@@ -12,15 +22,36 @@ include 'includes/navbar.php';
             <div class="card-header text-white text-center">
                 User List
             </div>
-            <div class="card-body text-success">
+            <div class="card-body">
                 <!-- User Table -->
                 <table class="table table-striped">
                     <tbody>
+                        <?php
+                            foreach($data_from_db as $user_value)
+                            {
+                                // print_r($user_value);
+                            // }
+                        ?>
                         <tr>
-                            <th>ID</th>
-                            <td>Name</td>
-                            <td>Email</td>
+                            <th>
+                                <?php
+                                    print_r($user_value['id']);
+                                ?>
+                            </th>
+                            <td>
+                            <?php
+                                    print_r($user_value['full_name']);
+                                ?>
+                            </td>
+                            <td>
+                            <?php
+                                    print_r($user_value['email_address']);
+                                ?>
+                            </td>
                         </tr>
+                        <?php
+                            }
+                        ?>
                     </tbody>
                 </table>
                 <!-- User Table -->
